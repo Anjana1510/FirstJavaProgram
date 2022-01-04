ea160a187ed2cbb7bdfff8c5087eef368bba41b6
Anjana1510 <borad.anju@gmail.com>
2022-01-04 21:19:08 +0000

    This is my first commit

---
+++ ManualTestingTopics.java
@@ -0,0 +1,39 @@
+package com.myfirstprogram;
+
+public class ManualTestingTopics {
+
+    public static void main(String[] args){
+
+        System.out.println("What is Software ?");
+        System.out.println("Software Risks");
+        System.out.println("What is Software Testing ?");
+        System.out.println("Benefits of Software Testing");
+        System.out.println("Who does Testing ?");
+        System.out.println("Qualification of tester");
+        System.out.println("What is Manual Testing ?");
+        System.out.println("SDLC");
+        System.out.println("STLC");
+        System.out.println("Challenges of Manual Testing");
+        System.out.println("Principles of Software Testing");
+        System.out.println("Test Plan and it’s benefits");
+        System.out.println("Test Plan Template");
+        System.out.println("Test Scenario + Template");
+        System.out.println("Test Case + Template");
+        System.out.println("Testing Techniques");
+        System.out.println("What is a bug?");
+        System.out.println("Bug Lifecycle and Bug Template");
+        System.out.println("Severity Vs Priority");
+        System.out.println("Testing Levels");
+        System.out.println("Static vs Dynamic Testing");
+        System.out.println("Exploratory Testing");
+        System.out.println("Ad-Hoc Testing");
+        System.out.println("Software Development Models");
+        System.out.println("  1. Waterfall");
+        System.out.println("  2. Iterative");
+        System.out.println("  3. V-Model");
+        System.out.println("  4. Agile");
+        System.out.println("TDD and BDD");
+
+
+    }
+}
