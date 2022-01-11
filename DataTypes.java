c982a97dcb66acd192b63c1c5510ac71cb34abe1
Anjana1510 <borad.anju@gmail.com>
2022-01-11 11:29:39 +0000

    New program added

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+package com.myfirstprogram;
+
+public class DataTypes {
+
+    public static void main(String[] args){
+
+    byte b = 12;
+    short s = 100;
+    float f = 12.45f;
+    char c ='A';
+    long l = 22333322L;
+    int i = 5;
+
+    System.out.println(b);
+    System.out.println(s);
+    System.out.println(f);
+    System.out.println(c);
+    System.out.println(l);
+    System.out.println(i);
+}
+}
+
